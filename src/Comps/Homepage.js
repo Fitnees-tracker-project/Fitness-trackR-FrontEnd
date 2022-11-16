386e164ab656e3104fd6d13474f4a458e1aeffad
ddawg776 <ddawg776@gmail.com>
2022-11-16 12:55:39 -0600

    Able to start building here

---
+++ src/Comps/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+import {Outlet} from 'react-router'
+
+const Homepage = () => {
+    return(
+        <div>
+            <p>Welcome to FitnessTrackR</p>
+            <p>Get fit today!</p>
+        </div>
+    )
+}
+
+export default Homepage;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
  import React, {useState, useEffect} from 'react';
+ import ReactDom from 'react-dom'
  import { createRoot } from 'react-dom/client';
+ import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+ import Homepage from './Comps/Homepage';
+ import ErrorPage from './Comps/Errorpage'
+import Page from './Comps/index'
+const route = createBrowserRouter([
+    {
+        path: '/',
+        element: <Homepage />,
+        errorElement: <ErrorPage />,
 
- const App = () => {
-    return (
-        <div>
-            <p>Hello World!</p>
-        </div>
-    )
- }
+        children:[
+            {
+                index: true,
+                element: <Page />
+            }
+        ]
+    }
+])
 
- const appElement = document.getElementById('app')
- const root = createRoot(appElement)
- root.render(<App />, appElement)
+
+ReactDom.render(<RouterProvider router={route} />, document.getElementById('app'))
