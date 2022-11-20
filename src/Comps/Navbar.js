d7977cd125ec73ae2dcf6aeb673868938ebf460f
John Tacuri <johnsteven1231@gmail.com>
2022-11-19 20:40:56 -0500

    half way navbar

--- src/Comps/Navbar.js
+++ src/Comps/Navbar.js
@@ -7,9 +7,10 @@ const Navbar = () => {
         <nav className='navbar'>
             <Link to='/' className='link'>Homepage</Link>
             <Link to='/routines' className='link'>Routines</Link>
-            <Link to='/activities' className='link'>Activities</Link>
+            <Link to='/Activities' className='link'>Activities</Link>
             <Link to='/login' className='link'>Login</Link>
             <Link to='/register' className='link'>Register</Link>
+            <Link to='/NewActivity' className='link'>Create New Activity</Link>
         </nav>
     )
 }
